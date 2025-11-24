b7ddb5f543569d0a419cdf37ed1de0e552132e16
Kaish Reza <kaishreza786@gmail.com>
2025-11-25 00:15:35 +0530

    enable spring web security

--- src/main/java/com/uber/authservices/config/SpringSecurityConfig.java
+++ src/main/java/com/uber/authservices/config/SpringSecurityConfig.java
@@ -2,18 +2,23 @@ package com.uber.authservices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/services/v1/auth/signup/*")
+                        .permitAll()).build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
